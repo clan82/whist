b5f4b8319c6ad3f0504efae07749b94b82ea5642
Claus Andersen <clan82@gmail.com>
2019-03-18 21:38:47 +0100

    Fix: Use scheme-less external URLs to use the same scheme as the request (http:// -> //)

--- lib/views/stats.php
+++ lib/views/stats.php
@@ -1,6 +1,6 @@
 
-<script src="http://code.highcharts.com/highcharts.js"></script>
-<script src="http://code.highcharts.com/modules/exporting.js"></script>
+<script src="//code.highcharts.com/highcharts.js"></script>
+<script src="//code.highcharts.com/modules/exporting.js"></script>
 
 <div id="container" style="min-width: 310px; height: 500px; margin: 0 auto"></div>
 
